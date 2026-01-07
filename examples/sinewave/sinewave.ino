8bd44b35cfdee0c28128fb99e0a1dc4066842cbb
Geoffrey T. Falk <topquark22@gmail.com>
2026-01-07 11:27:18 -0500

    use rate limit

--- examples/sinewave/sinewave.ino
+++ examples/sinewave/sinewave.ino
@@ -3,19 +3,19 @@
 #include <LiquidCrystal_I2C.h>
 
 #include "servodac.h"
-#include "gpioutils.h" 
+#include "gpioutils.h"
 
 // --- wiring pins (matching the original sketch defaults) ---
-const uint8_t PIN_CHARGE     = 5;   // charge pin (pulse high, then hi-Z)
-const uint8_t PIN_DISCHARGE  = 4;   // discharge pin (active-high)
-const uint8_t PIN_FEEDBACK   = A2;  // feedback pin
+const uint8_t PIN_CHARGE = 5;     // charge pin (pulse high, then hi-Z)
+const uint8_t PIN_DISCHARGE = 4;  // discharge pin (active-high)
+const uint8_t PIN_FEEDBACK = A2;  // feedback pin
 
-const uint8_t PIN_FREQUENCY = A3;   // connect to voltage divider  
+const uint8_t PIN_FREQUENCY = A3;  // connect to voltage divider
 
 // R1 = 2.2K
 // C1 = 470nf
-const float TAU = 1.0e-3; // time constant R1 * C1 (seconds)
-const float RD = 1000;    // discharge resistor (ohms)
+const float TAU = 1.0e-3;  // time constant R1 * C1 (seconds)
+const float RD = 1000;     // discharge resistor (ohms)
 
 const float MAX_FREQUENCY = 20.0f;
 
@@ -30,20 +30,28 @@ LiquidCrystal_I2C lcd(LCD_ADDR, LCD_WIDTH, LCD_HEIGHT);
 
 ServoDAC dac(PIN_CHARGE, PIN_DISCHARGE, PIN_FEEDBACK, TAU, RD);
 Dejitter pin(PIN_FREQUENCY, 2);
+RateLimiter lim(300.0f, 300.0f);
 
-unsigned long start_us;
+unsigned long start_ms;
 
 void setup() {
+  Serial.begin(115200);
   lcd.init();
   lcd.backlight();
   lcd.clear();
 
   dac.begin();
-  start_us = micros();
+
+  start_ms = millis();
+
+  int v_raw = pin.read();
+  float v = v_raw * Vin / 1023.0f;
+  lim.set_value(v);
+  lim.begin();
 }
 
 float toFrequency(float voltage) {
-  return MAX_FREQUENCY * (voltage / Vout); // TODO make exponential
+  return MAX_FREQUENCY * (voltage / Vout);  // TODO make exponential
 }
 
 float t1 = 0;
@@ -64,9 +72,10 @@ void loop() {
 
   int v_raw = pin.read();
   float v = v_raw * Vin / 1023.0f;
-
-  float t = (micros() - start_us) * 1.0e-6f;
-  float f = toFrequency(v);
+  float t = (millis() - start_ms) * 1.0e-3f;
+  float f_unlim = toFrequency(v);
+  float f = lim.read(f_unlim);
+  Serial.println(f);
 
   if (f == 0) {
     dac.update(Vout / 2);
